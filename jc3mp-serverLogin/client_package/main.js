d16fcf3b202bc4a7ba6c8faf2381b5a4fd217dcb
JacobWard <jacobward1995@gmail.com>
2017-01-18 18:35:09 -0500

    Add files via upload

--- jc3mp-serverLogin/client_package/main.js
+++ jc3mp-serverLogin/client_package/main.js
@@ -1,20 +1,11 @@
-/**
- * @overview package-servernews Main
- * @author Jacob 'theWebKeyGuy'
- * @copyright (c) theWebKeyGuy
- * @license
- */
-
 'use strict';
 
 const ui = new WebUIWindow('jc3mp-serverLogin', 'package://jc3mp-serverLogin/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 ui.autoResize = true;
 
 var playerName = jcmp.players.filter(p => p.localPlayer)[0].name;
-var playerID = jcmp.players.filter(p => p.localPlayer)[0].networkId;
 
 jcmp.ui.AddEvent('userinfo', JSON.stringify({
-	id: playerID,
 	name: playerName
 }));
 
